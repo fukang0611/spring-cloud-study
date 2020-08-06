e708761675f899adaf2bab3a3d4d5b7ae0ee548f
付康 <12547345@qq.com>
2020-08-06 15:05:28 +0800

    添加 cloud-consumer-order 模块，用于消费支付服务接口。

--- cloud-consumer-order80/src/main/java/com/fk/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/fk/springcloud/OrderMain80.java
@@ -3,13 +3,10 @@ package com.fk.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author fk
- */
 @SpringBootApplication
-public class PaymentMain8001 {
+public class OrderMain80 {
 
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(OrderMain80.class, args);
     }
 }
