9f599db525459502738db927631ddac67d6c01fd
付康 <12547345@qq.com>
2020-09-17 18:03:03 +0800

    nacos 配置中心。

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/fk/springcloud/SentinelServiceMain8401.java
@@ -0,0 +1,12 @@
+package com.fk.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelServiceMain8401 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelServiceMain8401.class, args);
+    }
+}
---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/fk/springcloud/controller/SentinelController.java
@@ -0,0 +1,18 @@
+package com.fk.springcloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SentinelController {
+
+    @GetMapping("/testA")
+    public String testA() {
+        return "test A";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        return "test B";
+    }
+}
