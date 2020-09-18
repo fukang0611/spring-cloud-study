33638dbbcd9d6c7b1dcc8335b69076d012875b7f
付康 <12547345@qq.com>
2020-09-18 15:04:12 +0800

    Sentinel 服务限流熔断。

--- cloudalibaba-sentinel-service8401/src/main/java/com/fk/springcloud/SentinelServiceMain8401.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/fk/springcloud/SentinelServiceMain8401.java
@@ -2,8 +2,10 @@ package com.fk.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SentinelServiceMain8401 {
 
     public static void main(String[] args) {
